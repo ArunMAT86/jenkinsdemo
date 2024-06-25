1cfb1732eb375a0a0b932721f3505a495f629b5a
Arun Mathew <arunmathew009@gmail.com>
2024-06-25 15:22:16 -0400

    second commit

--- jenkinsDemo/src/main/java/com/jenkinsDemo/Controller/MyController.java
+++ jenkinsDemo/src/main/java/com/jenkinsDemo/Controller/MyController.java
@@ -14,10 +14,19 @@ public class MyController {
     @GetMapping("displayAll")
     public List<Employee> getAllEmployees(){
         Employee emp1 = new Employee();
+        Employee emp2 = new Employee();
+        Employee emp3 = new Employee();
         emp1.setName("Arun");
         emp1.setEmail("arun@gmail.com");
+        emp2.setName("abc");
+        emp2.setEmail("abc@gmail.com");
+        emp3.setName("def");
+        emp3.setEmail("def@gmail.com");
+
         ArrayList<Employee> elist = new ArrayList<Employee>();
         elist.add(emp1);
+        elist.add(emp2);
+        elist.add(emp3);
         return elist;
     }
 }
